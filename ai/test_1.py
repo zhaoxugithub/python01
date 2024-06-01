0e81e12935d78d52c107691f0a88d122f7148c33
zhaoxu <18901842235@163.com>
2024-06-02 03:20:08 +0800

    提交

---
+++ ai/test_1.py
@@ -0,0 +1,10 @@
+import torch
+from diffusers import StableDiffusionXLPipeline, DPMSolverSinglestepScheduler
+
+if __name__ == '__main__':
+    pipe = StableDiffusionXLPipeline.from_pretrained("sd-community/sdxl-flash", torch_dtype=torch.float16,
+                                                     variant="fp16").to("cuda")
+    # Ensure sampler uses "trailing" timesteps.
+    pipe.scheduler = DPMSolverSinglestepScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
+    # Image generation.
+    pipe("a happy dog, sunny day, realism", num_inference_steps=7, guidance_scale=3).images[0].save("output.png")
